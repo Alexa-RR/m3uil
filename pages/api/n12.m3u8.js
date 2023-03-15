165f4d71b369637d263e0b28855f28590110bc0b
Amit Dvir <53471801+Alexa-RR@users.noreply.github.com>
2023-03-15 17:44:35 +0200

    daw

--- pages/api/n12.m3u8.js
+++ pages/api/n12.m3u8.js
@@ -1,3 +1,5 @@
+import HLS from 'hls-parser';
+
 async function getstreamticket() {
     const res = await fetch('https://mass.mako.co.il/ClicksStatistics/entitlementsServicesV2.jsp?et=gt&lp=/stream/hls/live/2033791/k12/index.m3u8&rv=AKAMAI');
     if (!res.ok) {
@@ -8,9 +10,24 @@ async function getstreamticket() {
       return res.json();    
 }
 
+async function getstream(ticket) {
+  const res = await fetch(`https://mako-streaming.akamaized.net/stream/hls/live/2033791/k12/index.m3u8?${ticket.tickets[0].ticket}`)
+  if (!res.ok) {
+    // This will activate the closest `error.js` Error Boundary
+    throw new Error('Failed to fetch data');
+  }
+  var output = res.text()
+  
+
+  return output;
+}
 
 export default async function handler(req, res) {
     const ticket = await getstreamticket()
+    const stream = await getstream(ticket)
+    const parse = HLS.parse(stream)
     res.status(301)
-    res.redirect(`https://mako-streaming.akamaized.net/stream/hls/live/2033791/k12/index.m3u8?${ticket.tickets[0].ticket}`)
+    //res.json(parse.variants[5].uri)
+    res.redirect(`https://mako-streaming.akamaized.net/stream/hls/live/2033791/k12/${parse.variants[5].uri}`)
+    //res.redirect(`https://mako-streaming.akamaized.net/stream/hls/live/2033791/k12/index.m3u8?${ticket.tickets[0].ticket}`)
   }
